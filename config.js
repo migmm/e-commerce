a7bed5fff161036129e3a8e1c47092b1b657380a
migmm <micmigm@gmail.com>
2022-10-09 14:52:53 -0300

    Switch to env

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+
 const PERSISTENCE_TYPE = {
-    TYPE_MEM: 'MEMORY',
-    TYPE_FILE: 'FILE SYSTEM',
     TYPE_MONGODB: 'MONGODB',
 };
 
 const config = {
-    PORT: 8080,
-    PERSISTENCE_TYPE: PERSISTENCE_TYPE.TYPE_MONGODB,    // 'MEM', 'FILE', 'MONGODB'
-    // MONGODB_CONNECTION_STR: 'mongodb://localhost/ecommerce',
-    MONGODB_CONNECTION_STR: 'mongodb+srv://databaseincome:2f7bMvxGVSY5cgY@cluster0.mm8881o.mongodb.net/cosmica?retryWrites=true&w=majority',
-    MONGODB_TIMEOUT: 3000,  // Valor bajo para TESTING
+    PORT: process.env.PORT,
+    PERSISTENCE_TYPE: PERSISTENCE_TYPE.TYPE_MONGODB, 
+    MONGODB_CONNECTION_STR: `mongodb+srv://${process.env.DB_NAME}:${process.env.PASSWORD}@cluster0.mm8881o.mongodb.net/${process.env.COLLECTION_NAME}?retryWrites=true&w=majority`,
+    MONGODB_TIMEOUT: 3000, 
 };
 
 export {PERSISTENCE_TYPE, config as default};
--- public/js/main.js
+++ public/js/main.js
@@ -38,6 +38,29 @@ async function renderTemplateCards(products) {
 
 async function initInicio() {
     console.log('initInicio()');
+    ////////////////////////////////
+    //                            //
+    //    //Pseudo hero slider    //
+    //                            //
+    ////////////////////////////////
+     //-Need improvements-
+
+    var slideIndex = 0;
+    carousel();
+
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+
+        slideIndex++;
+        if (slideIndex > x.length) {slideIndex = 1}
+        x[slideIndex-1].style.display = "block";
+        setTimeout(carousel, 2000);
+        x= false // Change image every 2 seconds
+    }
 
     const products = await productController.getProducts();
     renderTemplateCards(products);
