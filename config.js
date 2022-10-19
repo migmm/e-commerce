8f78e24afed28bf68514d76afae251ce4b32a371
migmm <micmigm@gmail.com>
2022-10-19 19:03:30 -0300

    Added temp features

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const config = {
     PORT: process.env.PORT,
     PERSISTENCE_TYPE: PERSISTENCE_TYPE.TYPE_MONGODB, 
     MONGODB_CONNECTION_STR: `mongodb+srv://${process.env.DB_NAME}:${process.env.PASSWORD}@cluster0.mm8881o.mongodb.net/${process.env.COLLECTION_NAME}?retryWrites=true&w=majority`,
-    MONGODB_TIMEOUT: 10000, 
+    MONGODB_TIMEOUT: 30000, 
 };
 
 export {PERSISTENCE_TYPE, config as default};
