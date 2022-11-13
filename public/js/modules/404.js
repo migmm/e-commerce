20471f51b41ca280cb5e0853aeb94e33a8988579
migmm <micmigm@gmail.com>
2022-11-13 15:42:16 -0300

    Improved 404

---
+++ public/js/modules/404.js
@@ -0,0 +1,11 @@
+console.log('🆗: Módulo Page404 cargado.');
+
+class Page404 {
+
+    static async init () {
+        console.log('Page404.init()');
+        //goToTopAndCloseMenu ();
+    }
+}
+
+export default Page404;
