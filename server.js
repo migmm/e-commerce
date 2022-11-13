ac98f15181394b26154934d9511620f4e69135c3
migmm <micmigm@gmail.com>
2022-11-13 15:24:38 -0300

    Added 404 redirect in backend

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.use(express.json());
 app.use('/api/products', routerProducts);
 app.use('/api/cart', routerCart);
 
+
+app.get("/*", function (req, res) {
+    res.redirect("/#/404");
+});
+
 const PORT = config.PORT;
 const server = app.listen(PORT, () => console.log(`Servidor Express escuchando en el puerto ${PORT}.`));
 server.on('error', error => console.log('Error al iniciar el servidor Express: ' + error.message));
