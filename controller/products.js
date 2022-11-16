da2e302c255d24b5e59012fbcae025476934c5fb
migmm <micmigm@gmail.com>
2022-11-16 16:05:56 -0300

    Code improve

--- controller/products.js
+++ controller/products.js
@@ -27,8 +27,7 @@ const postProduct = async function (req, res, next) {
 
     const product = req.body;
     const files = req.files;
-
-    let locationName = PRODUCT_IMG_UPLOAD_LOCATION.STORAGE_LOCATION.substring(9);
+    const locationName = PRODUCT_IMG_UPLOAD_LOCATION.STORAGE_LOCATION.substring(9);
 
     product['images'] = {};
 
@@ -64,39 +63,40 @@ const postProduct = async function (req, res, next) {
 const putProduct = async (req, res) => {
     const id = req.params.id;
     const product = req.body;
+    const files = req.files;
+    const locationName = PRODUCT_IMG_UPLOAD_LOCATION.STORAGE_LOCATION.substring(9);
 
-    if (req.file !== undefined) {
+    if (files.avatar !== undefined) {
+
         const firstProductImg = req.files['avatar'][0];
-        let productImgGallery = req.files['gallery'];
-
+        
         // const data = req.body
         /*     const previousAvatar = data.images.prevAvatar;
             const previousGalleryImg0 = data.images.prevGallery0
             const previousGalleryImg1 = data.images.prevGallery1
             const previousGalleryImg2 = data.images.prevGallery2 */
-        let locationName = storageLocation.substring(9);
 
-        product['images'] = {}
+       /*  product['images'] = {} */
+
         product['images']['portada'] = locationName + firstProductImg.filename;
+    } 
+    
+    if (files.gallery !== undefined) {
 
-        if (productImgGallery !== undefined) {
-
-            for (let i = 0; i <= productImgGallery.length; ++i) {
-                if (productImgGallery[i] !== undefined) {
-                    product['images'][`${'galeria' + [i]}`] = locationName + productImgGallery[i].filename;
-                }
+        const productImgGallery = req.files['gallery'];
+        for (let i = 0; i <= productImgGallery.length; ++i) {
+            if (productImgGallery[i] !== undefined) {
+                product['images'][`${'galeria' + [i]}`] = locationName + productImgGallery[i].filename;
             }
         }
+    }
 
-
-
-    } /* else {
+    /*  else {
         res.status(415).send('<h1>Se produjo un error.</h1>');
     } */
 
     const updatedProduct = await api.updateProduct(id, product) || {};
     res.json(updatedProduct);
-
 };
 
 
