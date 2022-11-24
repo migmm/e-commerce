4475cbf9f95541389ccf686e082af60783dad779
migmm <micmigm@gmail.com>
2022-11-24 18:41:08 -0300

    Products section working with ONLY view all products

--- public/js/modules/productos.js
+++ public/js/modules/productos.js
@@ -1,9 +1,22 @@
+import productController from '/js/controllers/product.js';
+import cartController from '/js/modules/cart.js';
 
 console.log('🆗: Módulo PageProductos cargado.');
 
 class PageProductos  {
 
+    static async renderTemplateCards(products) {
+        const hbsFile = await fetch('templates/inicio.hbs').then(r => r.text());
+        const template = Handlebars.compile(hbsFile);
+        const html = template({ products });
+        document.querySelector('.cards-container').innerHTML = html;
+    }
+
     static async init () {
+        const products = await productController.getProducts();
+        console.log(`Se encontraron ${products.length} productos`);
+        await PageProductos.renderTemplateCards(products);
+        await cartController.init()
         console.log('PageProductos .init()');
         //goToTopAndCloseMenu ();
     }
