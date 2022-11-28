e23eae70cb26086c34bd8a9c3a8e718a930f7c96
migmm <micmigm@gmail.com>
2022-11-27 21:39:23 -0300

    Temporary fix to card scroll

--- public/js/modules/inicio.js
+++ public/js/modules/inicio.js
@@ -38,39 +38,82 @@ class PageInicio {
         let isDown = false;
         let startX;
         let scrollLeft;
-        const slider = document.querySelector('.cards-container');
+        const slider0 = document.querySelectorAll('.cards-container')[0];
+        const slider1 = document.querySelectorAll('.cards-container')[1];
+        const slider2 = document.querySelectorAll('.cards-container')[2];
         
         const end = () => {
             isDown = false;
-            slider.classList.remove('active');
+            slider0.classList.remove('active');
+            slider1.classList.remove('active');
+            slider2.classList.remove('active');
         }
 
         const start = (e) => {
             isDown = true;
-            slider.classList.add('active');
-            startX = e.pageX || e.touches[0].pageX - slider.offsetLeft;
-            scrollLeft = slider.scrollLeft;
+
+            slider0.classList.add('active');
+            startX = e.pageX || e.touches[0].pageX - slider0.offsetLeft;
+            scrollLeft = slider0.scrollLeft;
+
+            slider1.classList.add('active');
+            startX = e.pageX || e.touches[0].pageX - slider1.offsetLeft;
+            scrollLeft = slider1.scrollLeft;
+
+            slider2.classList.add('active');
+            startX = e.pageX || e.touches[0].pageX - slider2.offsetLeft;
+            scrollLeft = slider2.scrollLeft;
         }
 
         const move = (e) => {
             if (!isDown) return;
 
             e.preventDefault();
-            const x = e.pageX || e.touches[0].pageX - slider.offsetLeft;
-            const dist = (x - startX);
-            slider.scrollLeft = scrollLeft - dist;
+            const x0 = e.pageX || e.touches[0].pageX - slider0.offsetLeft;
+            const dist0 = (x0 - startX);
+            slider0.scrollLeft = scrollLeft - dist0;
+
+            const x1 = e.pageX || e.touches[0].pageX - slider1.offsetLeft;
+            const dist1 = (x1 - startX);
+            slider1.scrollLeft = scrollLeft - dist1;
+
+            const x2 = e.pageX || e.touches[0].pageX - slider2.offsetLeft;
+            const dist2 = (x2 - startX);
+            slider2.scrollLeft = scrollLeft - dist2;
         }
 
         (() => {
-            slider.addEventListener('mousedown', start);
-            slider.addEventListener('touchstart', start);
+            slider0.addEventListener('mousedown', start);
+            slider0.addEventListener('touchstart', start);
 
-            slider.addEventListener('mousemove', move);
-            slider.addEventListener('touchmove', move);
+            slider1.addEventListener('mousedown', start);
+            slider1.addEventListener('touchstart', start);
 
-            slider.addEventListener('mouseleave', end);
-            slider.addEventListener('mouseup', end);
-            slider.addEventListener('touchend', end);
+            slider2.addEventListener('mousedown', start);
+            slider2.addEventListener('touchstart', start);
+
+
+            slider0.addEventListener('mousemove', move);
+            slider0.addEventListener('touchmove', move);
+
+            slider1.addEventListener('mousemove', move);
+            slider1.addEventListener('touchmove', move);
+
+            slider2.addEventListener('mousemove', move);
+            slider2.addEventListener('touchmove', move);
+
+
+            slider0.addEventListener('mouseleave', end);
+            slider0.addEventListener('mouseup', end);
+            slider0.addEventListener('touchend', end);
+
+            slider1.addEventListener('mouseleave', end);
+            slider1.addEventListener('mouseup', end);
+            slider1.addEventListener('touchend', end);
+
+            slider2.addEventListener('mouseleave', end);
+            slider2.addEventListener('mouseup', end);
+            slider2.addEventListener('touchend', end);
         })();
 
     }
