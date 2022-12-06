2a5ad51c15f5685af9f05514160565cf131273e8
migmm <micmigm@gmail.com>
2022-12-06 18:03:04 -0300

    Removed unused code

--- public/js/modules/alta.js
+++ public/js/modules/alta.js
@@ -4,14 +4,6 @@ import cartController from '/js/modules/cart.js';
 
 console.log('🆗: Módulo PageAlta cargado.');
 
-const imgSelect = document.querySelectorAll('.img-select__img-container');
-imgSelect.forEach(function (image) {
-    image.addEventListener('click', (e) => {
-        e.preventDefault();
-        const bigImg = document.querySelector('.img-display__img-big');
-        bigImg.src = e.target.src;
-    })
-})
 
 class PageAlta {
 
