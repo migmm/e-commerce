809c87dd805ad4724e491260da1cc1684b5a86b8
migmm <micmigm@gmail.com>
2023-01-09 10:50:24 -0300

    Code refactoring

--- public/js/modules/404.js
+++ public/js/modules/404.js
@@ -1,20 +1,11 @@
 import cartController from '/js/modules/cart.js';
+import goTopOnLoad from '../utils/goTopOnLoad.js';
 
-console.log('🆗: Módulo Page404 cargado.');
 
 class Page404 {
 
-    static goToTopOnLoad() {
-        const htmlTag = document.getElementsByTagName('html')[0];
-        htmlTag.classList.remove('scroll-smooth');
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-        htmlTag.classList.add('scroll-smooth');
-    }
-
     static async init () {
-        console.log('Page404.init()');
-        Page404.goToTopOnLoad();
+        goTopOnLoad.goToTopOnLoad();
         await cartController.init();
     }
 }
---
+++ public/js/utils/goTopOnLoad.js
@@ -0,0 +1,12 @@
+class goTopOnLoad {
+    
+    static goToTopOnLoad() {
+        const htmlTag = document.getElementsByTagName('html')[0];
+        htmlTag.classList.remove('scroll-smooth');
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+        htmlTag.classList.add('scroll-smooth');
+    }
+} 
+
+export default goTopOnLoad;
