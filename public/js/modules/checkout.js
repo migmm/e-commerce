4418c90d2ec85ce8492e5917b2198a78f25b13f4
migmm <micmigm@gmail.com>
2023-01-15 22:32:05 -0300

    Working checkout only in local storage

--- public/js/modules/checkout.js
+++ public/js/modules/checkout.js
@@ -7,14 +7,44 @@ class Checkout {
 
     static cart = [];
 
-    static async init () {
+    static commonEvents() {
+
+        const checkout = document.getElementsByClassName('cart-modal__footer__link-buy button-buy')[0]
+
+        checkout.addEventListener('click', async e => {
+            e.preventDefault();
+            
+            console.log('fdfddffdfdfd')
+
+            const loggedIn = true;
+            const user = 'arthemis';
+            let savedCart = [];
+            this.cart = []
+
+            //const cartLoaded = await cartService.loadCart();
+            localStorage.setItem('cart', JSON.stringify(savedCart));
+
+          /*   if (loggedIn) {
+
+                var index = cartLoaded.findIndex(item => item.userID === user);
+                const userID = cartLoaded[index].id;
+                savedCart.userID = user;
+                savedCart.cartContent = this.cart;
+                await cartService.updateCart(savedCart, userID);
+            } */
+
+            
+        })
+    }
+
+    static async init() {
         await cartController.init();
         this.cart = JSON.parse(localStorage.getItem('cart')) || [];
         await render.renderTemplateCards(this.cart, 'templates/card-cart-preview.hbs', '.checkout__products')
         cartController.updateCart();
         this.links = document.querySelectorAll('.main-nav__link');
         goTopOnLoad.goToTopOnLoad();
-        
+        this.commonEvents();
     }
 }
 
