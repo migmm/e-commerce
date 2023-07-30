40c9bb8b976072663df95cb6bf39aac874338f1d
migmm <micmigm@gmail.com>
2023-07-29 23:52:36 -0300

    Changed toast code

--- public/js/utils/toast.js
+++ public/js/utils/toast.js
@@ -1,31 +1,63 @@
-class ToastComponent  {
-
-    toastNotification(msg) {
-        let toastNotifications = document.getElementById("toast-notification");
-
-        if (toastNotifications.childElementCount < 2) {
-            let alertBox = document.createElement("div");
-            alertBox.classList.add("alert-msg", "slide-in");
-            let alertMsg = document.createTextNode(msg);
-            alertBox.appendChild(alertMsg);
-
-            toastNotifications.insertBefore(alertBox, toastNotifications.childNodes[0]);
-
-            if (toastNotifications.childElementCount > 1 ) {
-                console.log("hay mas de uno")
-                toastNotifications.childNodes[1].classList.add("slide-out")
-            } 
-
-            setTimeout(function () {
-                toastNotifications.removeChild(toastNotifications.lastChild);
-            }, 2900);
-        }
-    }
-
-    static async init () {
-        console.log('Module Toast.init()');
-    }
+function generateId() {
+    return '_' + Math.random().toString(36).substr(2, 9);
 }
 
-const toastComponent = new ToastComponent();
-export default toastComponent;
+function showToast(message, type, color, location) {
+    const toastContainer = document.getElementById('toast-container');
+    const toastElement = document.createElement('div');
+    const toastId = generateId();
+    toastElement.className = 'toast ' + location;
+    toastElement.innerHTML = `
+<div class="toast-content">
+    <div class="toast-icon">
+        <i class="fa fa-${getIconForType(type)} fa-lg"></i>
+    </div>
+    <div class="toast-text">${message}</div>
+    <div class="toast-close">
+        <i class="fa fa-times fa-lg"></i>
+    </div>
+</div>`;
+
+    toastElement.style.backgroundColor = color;
+    toastElement.setAttribute('id', toastId);
+
+    const closeButton = toastElement.querySelector('.toast-close');
+    closeButton.onclick = () => {
+        const toastToRemove = document.getElementById(toastId);
+        if (toastToRemove) {
+            toastToRemove.classList.remove('show');
+            setTimeout(() => {
+                toastContainer.removeChild(toastToRemove);
+            }, 300);
+        }
+    };
+
+    toastContainer.appendChild(toastElement);
+
+    setTimeout(() => {
+        toastElement.classList.add('show');
+    }, 100);
+
+    setTimeout(() => {
+        const toastToRemove = document.getElementById(toastId);
+        if (toastToRemove) {
+            toastToRemove.classList.remove('show');
+            setTimeout(() => {
+                toastContainer.removeChild(toastToRemove);
+            }, 300);
+        }
+    }, 3000);
+}
+
+function getIconForType(type) {
+    switch (type) {
+        case 'warning':
+            return 'exclamation-triangle';
+        case 'error':
+            return 'times-circle';
+        case 'success':
+            return 'check-circle';
+        default:
+            return 'info-circle';
+    }
+}
