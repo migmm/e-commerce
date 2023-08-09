0cc6ded39b2f3f8162d5f181d593f7e6dbace3c3
migmm <micmigm@gmail.com>
2023-08-09 16:41:08 -0300

    Fixed cart

--- public/js/modules/checkout.js
+++ public/js/modules/checkout.js
@@ -8,31 +8,21 @@ class Checkout {
     static cart = [];
 
     static commonEvents() {
-
-        // this.cart = JSON.parse(localStorage.getItem('cart')) || [];
-
         const checkout = document.getElementsByClassName('cart-modal__footer__link-buy button-buy')[0];
+        let partialPrice = document.getElementsByClassName('card-cart-preview__price__price-subtotal');
+        let favsTotal = document.getElementsByClassName('checkout__final');
         const modalOk = document.getElementsByClassName('main-content__modal-thanks-container')[0];
-        let chekoutTotal = document.getElementsByClassName('checkout__total');
-        let partialPrice = document.getElementsByClassName('card-checkout-preview__price__price-subtotal');
 
-
-        chekoutTotal[0].innerHTML = 0;
+        favsTotal[0].innerHTML = 0;
 
         for (let i = 0; i <= partialPrice.length - 1; ++i) {
-            chekoutTotal[0].innerHTML = parseInt(chekoutTotal[0].innerHTML) + parseInt(partialPrice[i].innerHTML)
+            favsTotal[0].innerHTML = parseInt(favsTotal[0].innerHTML) + parseInt(partialPrice[i].innerHTML);
         }
 
         checkout.addEventListener('click', async e => {
             e.preventDefault();
 
-            const loggedIn = true;
-            const user = 'arthemis';
-            let savedCart = [];
-            this.cart = []
-
-            //const cartLoaded = await cartService.loadCart();
-            localStorage.setItem('cart', JSON.stringify(savedCart));
+            localStorage.setItem('cart', JSON.stringify([]));
 
             modalOk.classList.add("visible-flex");
             function timedRedirect() {
@@ -41,25 +31,13 @@ class Checkout {
             }
 
             setTimeout(timedRedirect, 3000);
-
-              if (loggedIn) {
-    
-                    var index = cartLoaded.findIndex(item => item.userID === user);
-                    const userID = cartLoaded[index].id;
-                    savedCart.userID = user;
-                    savedCart.cartContent = this.cart;
-                    await cartService.updateCart(savedCart, userID);
-                } 
-           
-
         })
     }
 
     static async init() {
         await cartController.init();
         this.cart = JSON.parse(localStorage.getItem('cart')) || [];
-        const currentLang = 'en'
-        await render.renderTemplateCards(this.cart, 'templates/card-checkout-preview.hbs', '.checkout__products', currentLang)
+        await render.renderTemplateCards(this.cart, 'templates/card-checkout-preview.hbs', '.checkout__products')
         cartController.updateCart();
         this.links = document.querySelectorAll('.main-nav__link');
         goTopOnLoad.goToTopOnLoad();
