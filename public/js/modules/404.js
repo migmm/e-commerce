e5ed84de0a25093f17ad9653a5a657e0d1197cd1
migmm <micmigm@gmail.com>
2023-09-19 12:51:19 -0300

    Added lang tags

--- public/js/modules/404.js
+++ public/js/modules/404.js
@@ -1,5 +1,6 @@
 import cartController from '/js/modules/cart.js';
 import goTopOnLoad from '../utils/goTopOnLoad.js';
+import fetchLanguageData from '../utils/langFunctions.js'
 
 
 class Page404 {
@@ -7,6 +8,7 @@ class Page404 {
     static async init () {
         goTopOnLoad.goToTopOnLoad();
         await cartController.init();
+        await fetchLanguageData.fetchLanguageData();
     }
 }
 
