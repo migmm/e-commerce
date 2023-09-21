0fe128b216437826e3a386788464f1aac0bda759
migmm <micmigm@gmail.com>
2023-09-21 12:44:39 -0300

    Added docs to lang

--- helpers/swagger.js
+++ helpers/swagger.js
@@ -1,28 +1,30 @@
-import swaggerJSDoc from "swagger-jsdoc";
-import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 
 const options = {
     definition: {
-        openapi: "3.0.0",
-        info: { title: "Juguetería cósmica API", version: "1.0.0" }
+        openapi: '3.0.0',
+        info: { title: 'Juguetería cósmica API', version: '1.0.0' }
     },
     apis: [
-        "router/auth.js",
-        "router/products.js"
+        'router/auth.js',
+        'router/products.js',
+        'router/lang.js'
     ]
 };
 
 const swaggerSpec = swaggerJSDoc(options);
 
 const swaggerDocs = (app, port) => {
-    app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-    app.get("/api/docs.json", (req, res) => {
-        res.setHeader("Content-Type", "application/json");
+    app.get('/api/docs.json', (req, res) => {
+        res.setHeader('Content-Type', 'application/json');
         res.send(swaggerSpec);
     });
     console.log(`API Documentation available at /api/docs`);
 };
+
 
 export default swaggerSpec;
--- router/lang.js
+++ router/lang.js
@@ -3,7 +3,105 @@ const routerLang = express.Router();
 
 import { changeLanguage, getLanguageInfo } from '../controller/lang.js';
 
+
 routerLang.get('/availablelangs', getLanguageInfo);
+
+/**
+ * @openapi
+ * /api/lang/changelanguage/{language}:
+ *   get:
+ *     summary: Get UI language translation data.
+ *     tags: [Localization]
+ *     parameters:
+ *       - in: path
+ *         name: language
+ *         required: false
+ *         description: The desired language for UI translations (e.g., 'en' for English).
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved UI language translation data.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 language:
+ *                   type: string
+ *                   description: The language name (e.g., 'English').
+ *                 language-abbreviation:
+ *                   type: string
+ *                   description: The language abbreviation (e.g., 'EN').
+ *                 main-header:
+ *                   type: object
+ *                   description: Translation data for the main header section.
+ *                   properties:
+ *                     header-logo-alt:
+ *                       type: string
+ *                     search-input-placeholder:
+ *                       type: string
+ *                     view-search-results:
+ *                       type: string
+ *                     hamburger-button-title:
+ *                       type: string
+ *                 user-button:
+ *                   type: object
+ *                   description: Translation data for user buttons.
+ *                   properties:
+ *                     signin:
+ *                       type: string
+ *                     signup:
+ *                       type: string
+ *                 navbar:
+ *                   type: object
+ *                   description: Translation data for the navigation bar.
+ *                   properties:
+ *                     home:
+ *                       type: string
+ *                     products:
+ *                       type: string
+ *                     about-us:
+ *                       type: string
+ *                     contact:
+ *                       type: string
+ *                     login:
+ *                       type: string
+ *                     register:
+ *                       type: string
+ *                     new-product:
+ *                       type: string
+ *                     edit-product:
+ *                       type: string
+ *                 cart-preview:
+ *                   type: object
+ *                   description: Translation data for the cart preview.
+ *                   properties:
+ *                     your-cart:
+ *                       type: string
+ *                     check-out:
+ *                       type: string
+ *                     continue-shopping:
+ *                       type: string
+ *                     empty-cart:
+ *                       type: string
+ *                     price-with-sign:
+ *                       type: string
+ *                     subtotal:
+ *                       type: string
+ *       500:
+ *         description: Internal Server Error indicating an error while retrieving language data.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message.
+ *                   example: 'Error retrieving language data'
+ */
 routerLang.post('/changeLanguage/:language?', changeLanguage);
+
 
 export default routerLang;
