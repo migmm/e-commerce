19d55106440ff90a9a4c7880fc649e9d1eea00eb
migmm <micmigm@gmail.com>
2023-09-21 12:33:32 -0300

    Added docs to postProduct

--- router/products.js
+++ router/products.js
@@ -207,6 +207,245 @@ routerProducts.get('/:lang?', productsController.getProducts);
  */
 routerProducts.get('/:id/:lang', productsController.getProduct);
 
+/**
+ * @openapi
+ * /api/products:
+ *   post:
+ *     summary: Create a new product.
+ *     tags: [Products]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               productData:
+ *                 type: string
+ *                 format: binary
+ *                 description: |
+ *                   JSON data containing product details.
+ *                   
+ *                   Example JSON data:
+ *                   ```json
+ *                   {
+ *                     "id": "64e3d1095e958d300be4b5a2",
+ *                     "productName": {
+ *                       "es": "Nombre del producto",
+ *                       "en": "Product Name"
+ *                     },
+ *                     "price": {
+ *                       "usd": 10.99,
+ *                       "eur": 9.99
+ *                     },
+ *                     "tax": 5.5,
+ *                     "vat": 21.0,
+ *                     "discountPercent": 10,
+ *                     "vendor": "Proveedor A",
+ *                     "stock": 100,
+ *                     "category": "Categoría",
+ *                     "shortDescription": {
+ *                       "es": "Descripción corta en español",
+ *                       "en": "Short description in English"
+ *                     },
+ *                     "longDescription": {
+ *                       "es": "Descripción larga en español",
+ *                       "en": "Long description in English"
+ *                     },
+ *                     "freeShip": true,
+ *                     "ageFrom": 5,
+ *                     "ageTo": 12,
+ *                     "ageSelect": "Rango de edad",
+ *                     "addedDate": "2023-09-20T12:00:00Z",
+ *                     "status": true,
+ *                     "lastSell": "2023-09-21T14:30:00Z",
+ *                     "lastModified": "2023-09-21T10:15:00Z",
+ *                     "views": 1000,
+ *                     "lastVisited": "2023-09-21T15:45:00Z",
+ *                     "images": ["url1.jpg", "url2.jpg"],
+ *                     "colors": ["Red", "Blue"],
+ *                     "urlName": "nombre-del-producto"
+ *                   }
+ *                   ```
+ *               files:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *                   format: binary
+ *                 description: Product images.
+ *     responses:
+ *       201:
+ *         description: Successfully created a new product.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                   description: Unique product identifier.
+ *                   example: '64e3d1095e958d300be4b5a2'
+ *                 productName:
+ *                   type: object
+ *                   properties:
+ *                     es:
+ *                       type: string
+ *                       description: Product name in Spanish.
+ *                       example: 'Nombre del producto'
+ *                     en:
+ *                       type: string
+ *                       description: Product name in English.
+ *                       example: 'Product Name'
+ *                 price:
+ *                   type: object
+ *                   description: Product price in multiple currencies.
+ *                   example:
+ *                     {
+ *                       usd: 10.99,
+ *                       eur: 9.99
+ *                     }
+ *                 tax:
+ *                   type: number
+ *                   description: Tax rate applied to the product.
+ *                   example: 5.5
+ *                 vat:
+ *                   type: number
+ *                   description: VAT (Value Added Tax) rate applied to the product.
+ *                   example: 21.0
+ *                 discountPercent:
+ *                   type: number
+ *                   description: Discount percentage applied to the product.
+ *                   example: 10
+ *                 vendor:
+ *                   type: string
+ *                   description: Product vendor or supplier.
+ *                   example: 'Proveedor A'
+ *                 stock:
+ *                   type: number
+ *                   description: Available stock quantity.
+ *                   example: 100
+ *                 category:
+ *                   type: string
+ *                   description: Product category.
+ *                   example: 'Categoría'
+ *                 shortDescription:
+ *                   type: object
+ *                   properties:
+ *                     es:
+ *                       type: string
+ *                       description: Short product description in Spanish.
+ *                       example: 'Descripción corta en español'
+ *                     en:
+ *                       type: string
+ *                       description: Short product description in English.
+ *                       example: 'Short description in English'
+ *                 longDescription:
+ *                   type: object
+ *                   properties:
+ *                     es:
+ *                       type: string
+ *                       description: Detailed product description in Spanish.
+ *                       example: 'Descripción larga en español'
+ *                     en:
+ *                       type: string
+ *                       description: Detailed product description in English.
+ *                       example: 'Long description in English'
+ *                 freeShip:
+ *                   type: boolean
+ *                   description: Indicates if shipping is free for the product.
+ *                   example: true
+ *                 ageFrom:
+ *                   type: number
+ *                   description: Minimum age requirement for the product.
+ *                   example: 5
+ *                 ageTo:
+ *                   type: number
+ *                   description: Maximum age requirement for the product.
+ *                   example: 12
+ *                 ageSelect:
+ *                   type: string
+ *                   description: Age selection criteria for the product.
+ *                   example: 'Rango de edad'
+ *                 addedDate:
+ *                   type: string
+ *                   format: date-time
+ *                   description: Date when the product was added.
+ *                   example: '2023-09-20T12:00:00Z'
+ *                 status:
+ *                   type: boolean
+ *                   description: Product status (active/inactive).
+ *                   example: true
+ *                 lastSell:
+ *                   type: string
+ *                   format: date-time
+ *                   description: Date of the last sale of the product.
+ *                   example: '2023-09-21T14:30:00Z'
+ *                 lastModified:
+ *                   type: string
+ *                   format: date-time
+ *                   description: Date when the product was last modified.
+ *                   example: '2023-09-21T10:15:00Z'
+ *                 views:
+ *                   type: number
+ *                   description: Number of views for the product.
+ *                   example: 1000
+ *                 lastVisited:
+ *                   type: string
+ *                   format: date-time
+ *                   description: Date when the product was last visited.
+ *                   example: '2023-09-21T15:45:00Z'
+ *                 images:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                   description: Array of product image URLs.
+ *                   example: ['url1.jpg', 'url2.jpg']
+ *                 colors:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                   description: Array of available product colors.
+ *                   example: ['Red', 'Blue']
+ *                 urlName:
+ *                   type: string
+ *                   description: URL-friendly product name.
+ *                   example: 'nombre-del-producto'
+ *       400:
+ *         description: Bad Request indicating an error while creating the product.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message.
+ *                   example: 'Error creating the product'
+ *       415:
+ *         description: Unsupported Media Type indicating an issue with the request format.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message.
+ *                   example: 'Unsupported media type'
+ *       500:
+ *         description: Internal Server Error indicating a server-side error while creating the product.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message.
+ *                   example: 'Error creating the product'
+ */
+
+
 routerProducts.post('/', upload.array('images', 10), handleMulterError, resizeImagesMiddleware, productsController.postProduct);
 
 routerProducts.put('/:id', upload.array('images', 10), handleMulterError, resizeImagesMiddleware, productsController.putProduct);
