f23b493bce5123c30f4c11830a75bb92accfe2d6
migmm <micmigm@gmail.com>
2023-10-22 00:54:26 -0300

    Fixed currency selector

--- public/js/utils/currencies.js
+++ public/js/utils/currencies.js
@@ -1,22 +1,24 @@
 const currencySet = async () => {
-    const currencySelect = document.getElementsByClassName('currency-select')[0];
+    const currencySelects = document.querySelectorAll('.currency-select');
 
-    if (localStorage.getItem('currency')) {
-        const currentCurrency = localStorage.getItem('currency');
-        
-        for (let i = 0; i < currencySelect.options.length; i++) {
-            if (currencySelect.options[i].value === currentCurrency) {
-                currencySelect.options[i].selected = true;
-                break;
+    for (const currencySelect of currencySelects) {
+        if (localStorage.getItem('currency')) {
+            const currentCurrency = localStorage.getItem('currency');
+
+            for (let i = 0; i < currencySelect.options.length; i++) {
+                if (currencySelect.options[i].value === currentCurrency) {
+                    currencySelect.options[i].selected = true;
+                    break;
+                }
             }
         }
-    }
 
-    currencySelect.addEventListener('change', () => {
-        const selectedCurrency = currencySelect.value;
-        localStorage.setItem('currency', selectedCurrency);
-        location.reload();
-    });
+        currencySelect.addEventListener('change', () => {
+            const selectedCurrency = currencySelect.value;
+            localStorage.setItem('currency', selectedCurrency);
+            location.reload();
+        });
+    }
 };
 
 export default {
