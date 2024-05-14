7f61a15b6bc885dcce5e1994fce5aa5b5970e6b4
Miguel <micmigm@gmail.com>
2024-05-14 18:47:26 -0300

    Added timezone selector in logger

--- middlewares/loggerMiddleware.js
+++ middlewares/loggerMiddleware.js
@@ -11,8 +11,13 @@ if (!fs.existsSync(logsDirectory)) {
 const appendFileAsync = util.promisify(fs.appendFile);
 
 const loggerMiddleware = async (req, res, next) => {
+
+    const timeZoneOffsetHours = -3;
     const currentDate = new Date();
-    const formattedDate = currentDate.toISOString().replace('T', ' ').slice(0, -5);
+
+    currentDate.setHours(currentDate.getHours() + timeZoneOffsetHours);
+
+    const formattedDate = currentDate.toISOString().replace('T', ' ').slice(0, -5) + ` GMT${timeZoneOffsetHours >= 0 ? '+' : ''}${timeZoneOffsetHours}`;
 
     let logMessage = `${formattedDate} - Request received: ${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`;
 
